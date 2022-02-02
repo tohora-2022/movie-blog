873a8e7500a6bb700ec735e40ff47bd625aa91a0
deepti-khanolkar <deepti.khanolkar@gmail.com>
2022-02-02 13:41:56 +1300

    Initial commit

---
+++ client/api.js
@@ -0,0 +1,9 @@
+import request from 'superagent'
+
+const widgetUrl = '/api/v1/widgets/'
+
+export function getWidgets () {
+  return request
+    .get(widgetUrl)
+    .then(response => response.body)
+}
